1f853f8a1f76a854d7f9b8448dfa8897125396b6
Atefeh Bakhshi <at.bakhshi1373@gmail.com>
2022-11-16 12:11:37 +0330

    style: opening and closing menu

--- script10.js
+++ script10.js
@@ -1,21 +1,21 @@
 'use strict'
-const menu = document.querySelector('#menu');
-const div = document.createElement('div');
-menu.addEventListener('click', () => {
-    document.body.style.cssText = `
-    overflow: auto;
-    `
-    div.style.cssText = `
-    display:block;
-    height:100vh;
-    width:100vw;
-    background-color:red;
-    position:absolute;
-    top:0;
-    left:0;
-    opacity:0.6;
-    transition: transform .3s cubic-bezier(0,.52,0,1);
-    `
-    document.body.append(div);
-    console.log('first')
-})
+
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+const myName = document.getElementById("myname");
+
+openMenu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+    openMenu.classList.add('invisible');
+    closeMenu.classList.toggle('hidden');
+    closeMenu.classList.toggle('flex');
+    myName.classList.add('invisible');
+});
+
+closeMenu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+    openMenu.classList.remove('invisible');
+    closeMenu.classList.toggle('hidden');
+    myName.classList.remove('invisible');
+});
